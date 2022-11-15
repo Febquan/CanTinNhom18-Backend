b46d611bfa47331c3d2efb1bc0e24b117fc17aed
Febquan <febquan1@gmail.com>
2022-11-15 10:49:55 +0700

    cors2

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const mongoose = require("mongoose");
-const cors = require(cors);
+const cors = require("cors");
 //cors
 app.use(cors());
 app.options("*", cors());
